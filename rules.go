c6ae0d848d3ce4966529e8fe1ab27811eec901ce
Mohamed Elbanhawi <128550106+mobanhawi@users.noreply.github.com>
2025-09-29 19:50:20 +1000

    fix: improve fail-fast policy handling in rule evaluation (#10)
    * fix: improve fail-fast policy handling in rule evaluation
    
    * fix: enhance rule evaluation error handling and documentation

--- rules.go
+++ rules.go
@@ -6,17 +6,26 @@ import (
 
 // RuleResult represents the outcome of a single rule evaluation
 type RuleResult struct {
+	// RuleName is the name of the evaluated rule
 	RuleName string
-	Passed   bool
-	Error    error
+	// Passed indicates whether the rule evaluation was successful
+	Passed bool
+	// Error contains the reason for rule not passing, if any, evaluation errors are not returned here
+	Error error
+	// Duration is the time taken to evaluate the rule
 	Duration time.Duration
 }
 
 // RulesetResult represents the outcome of a ruleset evaluation
 type RulesetResult struct {
+	// RulesetName is the name of the evaluated ruleset
 	RulesetName string
-	Passed      bool
+	// Passed indicates whether the ruleset evaluation was successful
+	Passed bool
+	// RuleResults contains the results of individual rule evaluations within the ruleset
 	RuleResults map[string]RuleResult
-	Error       error
-	Duration    time.Duration
+	// Error contains the reason for ruleset not passing, if any, evaluation errors are not returned here
+	Error error
+	// Duration is the time taken to evaluate the ruleset
+	Duration time.Duration
 }
